d5e5ce885e45ffaaefa60d784a419f8d80362869
urauymanov <urauymanov@yandex.ru>
2021-05-23 16:17:58 +0700

    configurating endpoints

--- DewarpingApp/Startup.cs
+++ DewarpingApp/Startup.cs
@@ -30,10 +30,7 @@ namespace DewarpingApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
