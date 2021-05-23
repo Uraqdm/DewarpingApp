36e1ff1c59075600d071f21fca93f2dd9d285814
urauymanov <urauymanov@yandex.ru>
2021-05-23 16:47:02 +0700

    Adding save image nethod(but without  realisation)

--- DewarpingApp/Services/ImageService.cs
+++ DewarpingApp/Services/ImageService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace DewarpingApp.Services
 {
     public class ImageService
     {
-        
+        public string SaveImage(IFormFile file)
+        {
+            throw new Exception();
+        }
     }
 }
