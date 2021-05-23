61ad5b584ccc7f605f82179a0e76c861c32c59a7
urauymanov <urauymanov@yandex.ru>
2021-05-23 16:14:59 +0700

    adding home controller

---
+++ DewarpingApp/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DewarpingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
