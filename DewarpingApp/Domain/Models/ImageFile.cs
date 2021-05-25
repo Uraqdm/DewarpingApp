8743f57020f3f2f9f3d043219d4e4dd6df96112f
urauymanov <urauymanov@yandex.ru>
2021-05-25 12:57:35 +0700

    adding image file model

---
+++ DewarpingApp/Domain/Models/ImageFile.cs
@@ -0,0 +1,9 @@
+﻿namespace DewarpingApp.Domain.Models
+{
+    public class ImageFile
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Name { get; set; }
+    }
+}
