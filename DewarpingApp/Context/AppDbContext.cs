fe0cb58d2e60e7eab487dd40f2a061427c3cb770
urauymanov <urauymanov@yandex.ru>
2021-05-25 13:36:03 +0700

    adding db context

---
+++ DewarpingApp/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DewarpingApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DewarpingApp.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ImageFile> ImageFiles { get; set; }
+    }
+}
