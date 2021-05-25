a23958222b75cbfe6aebc2499b80af022e6e7e93
urauymanov <urauymanov@yandex.ru>
2021-05-25 13:46:13 +0700

    adding context dependency

--- DewarpingApp/Controllers/HomeController.cs
+++ DewarpingApp/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DewarpingApp.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DewarpingApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext context;
+
+        public HomeController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index() => View();
     }
 }
