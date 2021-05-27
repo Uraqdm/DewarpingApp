6a7db051e0d11ee6ea17e6bf6876975fcb5028cd
urauymanov <urauymanov@yandex.ru>
2021-05-27 15:57:14 +0700

    adding possibility to add user's files in local server derictory

--- DewarpingApp/Controllers/HomeController.cs
+++ DewarpingApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using DewarpingApp.Context;
+using DewarpingApp.Service;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +9,14 @@ namespace DewarpingApp.Controllers
     public class HomeController : Controller
     {
         private readonly AppDbContext context;
+        private readonly IWebHostEnvironment environment;
+        private readonly FileService fileService;
 
-        public HomeController(AppDbContext context)
+        public HomeController(AppDbContext context, IWebHostEnvironment environment, FileService fileService)
         {
             this.context = context;
+            this.environment = environment;
+            this.fileService = fileService;
         }
 
         public IActionResult Index() => View();
@@ -19,6 +25,7 @@ namespace DewarpingApp.Controllers
         {
             if(file != null)
             {
+                fileService.SaveFileAsync(file, environment);
                 context.ImageFiles.Add(new Domain.Models.ImageFile { Name = file.FileName, Path = $"/Files/{file.FileName}" });
                 RedirectToAction("Index");
             }
