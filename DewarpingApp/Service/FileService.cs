5345a7a07eff72b47a91d3a1a192f5625b8062f4
urauymanov <urauymanov@yandex.ru>
2021-05-30 18:52:23 +0700

    making class and method static

--- DewarpingApp/Service/FileService.cs
+++ DewarpingApp/Service/FileService.cs
@@ -4,9 +4,9 @@ using System.IO;
 
 namespace DewarpingApp.Service
 {
-    public class FileService
+    public static class FileService
     {
-        public async void SaveAndTransformFileAsync(IFormFile file, IWebHostEnvironment environment)
+        public static async void SaveAndTransformFileAsync(IFormFile file, IWebHostEnvironment environment)
         {
             using var fileStream = new FileStream(environment.WebRootPath + "/Files/" + file.FileName, FileMode.Create);
             await file.CopyToAsync(fileStream);
