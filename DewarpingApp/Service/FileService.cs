50808b5b5bbcb0352bc2fce63fde386ac5df1d5e
urauymanov <urauymanov@yandex.ru>
2021-05-30 18:51:50 +0700

    rename method

--- DewarpingApp/Service/FileService.cs
+++ DewarpingApp/Service/FileService.cs
@@ -6,7 +6,7 @@ namespace DewarpingApp.Service
 {
     public class FileService
     {
-        public async void SaveFileAsync(IFormFile file, IWebHostEnvironment environment)
+        public async void SaveAndTransformFileAsync(IFormFile file, IWebHostEnvironment environment)
         {
             using var fileStream = new FileStream(environment.WebRootPath + "/Files/" + file.FileName, FileMode.Create);
             await file.CopyToAsync(fileStream);
