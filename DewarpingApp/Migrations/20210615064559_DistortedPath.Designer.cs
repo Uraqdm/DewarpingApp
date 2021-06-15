8f1ad80270d5b494a9835dcf9ecc59f8b9104697
urauymanov <urauymanov@yandex.ru>
2021-06-15 13:47:02 +0700

    Adding migration to change model in db

--- DewarpingApp/Migrations/20210615064559_DistortedPath.Designer.cs
+++ DewarpingApp/Migrations/20210615064559_DistortedPath.Designer.cs
@@ -3,14 +3,16 @@ using DewarpingApp.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DewarpingApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210615064559_DistortedPath")]
+    partial class DistortedPath
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DewarpingApp/Migrations/20210615064559_DistortedPath.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DewarpingApp.Migrations
+{
+    public partial class DistortedPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DistortedPath",
+                table: "ImageFiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DistortedPath",
+                table: "ImageFiles");
+        }
+    }
+}
